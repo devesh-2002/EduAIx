c0b3fa7467295054e1a1be6d73c4b9eab15f5176
dev241202@gmail.com <dev241202@gmail.com>
2024-07-10 08:55:40 +0530

    Added 3d classroom model using react three fiber

---
+++ client/src/components/classroomModel.jsx
@@ -0,0 +1,26 @@
+import { Canvas } from 'react-three-fiber';
+import { Suspense } from 'react';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  
+  return <primitive object={scene} />;
+}
+
+function ClassroomModel() {
+  return (
+    <div style={{ height: '100vh', margin:'10em' }}>
+      <Canvas camera={{ position: [0, 1, 3], fov: 60 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight position={[10, 50, 0]} angle={0.6} penumbra={1} />
+        <Suspense fallback={null}>
+          <Model url="classroom/scene.gltf" />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  )
+}
+
+export default ClassroomModel

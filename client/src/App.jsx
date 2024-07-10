d66aee9081f56e3390802c1a5ff4e30db7f7a608
dev241202@gmail.com <dev241202@gmail.com>
2024-07-10 09:16:34 +0530

    Integrated chakra UI with React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import ClassroomModel from "./components/classroomModel";
+import { ChakraProvider,Heading, Center,Text} from '@chakra-ui/react'
 function App() {
   return (
     <div>
+      <Heading><Center>EduAIx</Center></Heading>
       <ClassroomModel />
     </div>
     );
